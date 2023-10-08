ad4ef4d4bd03550016ee57cf915fbf616915ce75
Igor Santos <igorsantos9513@outlook.com>
2023-10-08 14:30:44 -0300

    prime fix

--- class_07/isPrime.js
+++ class_07/isPrime.js
@@ -1,14 +1,17 @@
 function isPrime(n){
-    //if is possible to divide return false
-    if(n < 2)return false
+  if(n < 2){
+      return false
+  }
 
-    for(let i = 2; i < n; i++){
-        if(n % i == 1){
-            return true
-        }else return false
-    }
+  for(let i = 2; i < n; i++){
+      if(n % i === 0){
+          return false
+      }
+  }
+
+  return true
 }
 
 console.log(isPrime(1))
-console.log(isPrime(5))
-console.log(isPrime(4))
+console.log(isPrime(7))
+console.log(isPrime(15))
